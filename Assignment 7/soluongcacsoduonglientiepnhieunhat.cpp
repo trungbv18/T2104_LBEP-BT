f4760f6140a663d300b9cb23ecc73a13d48f6232
Bui Van Trung <84084472+trungmark@users.noreply.github.com>
2021-06-02 12:19:50 +0700

    Add files via upload

---
+++ Assignment 7/soluongcacsoduonglientiepnhieunhat.cpp
@@ -0,0 +1,26 @@
+// 3. Nh?p m?ng n s? nguyên, tìm s? lu?ng các s? duong liên ti?p nhi?u nh?t
+#include<stdio.h>
+int main(){
+	int n;
+	printf("Nhap vao so phan tu cua mang: ");
+	scanf("%d", &n);
+	int arr[n];
+	for (int i=0;i<n;i++){
+		printf("Phan tu thu %d trong mang= ",i);
+		scanf("%d", &arr[i]);
+	}
+	int count=0; 
+	int count1=0;
+	for (int i=0;i<n;i++){
+		if (arr[i]>0) {
+			count+=arr[i];
+		}
+		else{
+			count=0;
+			}
+		if(count>count1){
+			count1=count;
+			}
+	}
+	printf("So luong cac so duong lien tiep nhieu nhat= %d",count1);
+}
