af8415dcf4c27039e8b6f72ac6a0e498f8949c34
Bui Van Trung <84084472+trungmark@users.noreply.github.com>
2021-06-03 16:16:03 +0700

    Nop lab s10

--- sapxepchuoitheoalphabettrcode.cpp
+++ sapxepchuoitheoalphabettrcode.cpp
@@ -9,7 +9,12 @@ void nhapmangstring(char arrs[][50],int n){
 		nhapstring(arrs[i]);
 	}
 }
-void sapxepalphabe(char arrs[][50],int n){
+int main(){
+	int n;
+	printf("Nhap so chuoi n=");
+	scanf("%d",&n);
+	char arrs[n][50];
+	nhapmangstring(arrs,n);
 	for(int i=0;i<n-1;i++){
 		for(int j=0;j<n-i-1;j++){
 			if(strcmp(arrs[j],arrs[j+1]) >0){
@@ -20,21 +25,8 @@ void sapxepalphabe(char arrs[][50],int n){
 			}
 		}
 	}
-}
-void inmangstring(char arrs[][50],int n){
+	printf("Sau khi sap xep theo bang alphabet:\n");
 	for(int i=0;i<n;i++){
 		printf("%s\n",arrs[i]);
 	}
-}
-
-
-int main(){
-	int n;
-	printf("Nhap n=");
-	scanf("%d",&n);
-	char arrs[n][50];
-	nhapmangstring(arrs,n);
-	sapxepalphabe(arrs,n);
-	printf("Sau khi sap xep theo bang alphabet:\n");
-	inmangstring(arrs,n);
 }
